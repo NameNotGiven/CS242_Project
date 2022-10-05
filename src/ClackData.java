7ac755912924e1b75b6e0395738852b8dbc1bb1c
polarolo <polaroal@clarkson.edu>
2022-10-05 17:34:26 -0400

    Commit

--- src/ClackData.java
+++ src/ClackData.java
@@ -1,2 +1,43 @@
 public class ClackData {
+    private String username;
+    private int type;
+    private Date date;
+public class Date{
+
+}
+public ClackData(String userName, int type){
+    this.username = userName;
+    this.type = type;
+    this.date = date;
+}
+
+public ClackData(int type){
+    this.username = "Anon";
+}
+
+public ClackData(){
+
+}
+
+
+
+    //accessor returns type
+public int getType(){
+    return type;
+}
+//accessor returns username
+public String getUserName(){
+    return username;
+}
+//accessor returns date
+public Date getDate(){
+    return date;
+}
+
+    /**abstract method to return data
+     */
+    public abstract getData(){
+
+}
+
 }
