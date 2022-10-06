b16fae594f5db8772bb39a3a9148c11044529abb
polarolo <polaroal@clarkson.edu>
2022-10-06 15:39:27 -0400

    Commit

--- src/FileClackData.java
+++ src/FileClackData.java
@@ -4,14 +4,16 @@ private String fileName;
 private String fileContents;
 
 /**
- constructor sets up username, fileName, and type
+ * constructor sets up username, fileName, and type
  */
 public FileClackData(String username, String fileName, int type){
     super(username, type);
     this.fileName = fileName;
     this.fileContents = null;
 }
-
+/**
+ * default constructor sets up fileName and fileContents to null, and type to 0
+ */
 public FileClackData(){
     super(0);
     this.fileName = null;
@@ -23,11 +25,16 @@ public void setFileName(String fileName){
     this.fileName = fileName;
 }
 
+/**
+ * return filename
+ * */
 public String getFileName(){
     return fileName;
 }
 
-
+/**
+ * return fileContents
+ * */
 public String getData(){
     return fileContents;
 }
@@ -39,11 +46,15 @@ public void readFileContents(){
 public void writeFileContents(){
 
 }
-
+/**
+ * return hashcode
+ * */
 public int hashCode(){
     return 0x123;
 }
-
+/**
+ * returns true if username, type, filename, and file contents are all the same
+ * */
 public boolean equals(Object other){
     FileClackData otherFile = (FileClackData)other;
     return
@@ -53,6 +64,9 @@ public boolean equals(Object other){
     otherFile.fileContents == this.fileContents;
 } //not sure if it needs dates to be equal or not
 
+/**
+ * returns a string with data, filename, file contents, username, and type
+ * */
 public String toString(){
     return "The date of the file is: " + super.getDate() + "\n" +
             "The name of the file is: " + this.fileName + "\n" +
