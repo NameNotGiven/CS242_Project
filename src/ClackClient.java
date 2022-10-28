208b7489959985cf687b137ac9cd568cf161ccfc
polarolo <polaroal@clarkson.edu>
2022-10-28 01:59:05 -0400

    Commit

--- src/ClackClient.java
+++ src/ClackClient.java
@@ -22,9 +22,21 @@ public class ClackClient {
  * dataToReceiveFromServer to null
  * */
     public ClackClient(String userName, String hostName, int port){
-    this.userName = userName;
-    this.hostName = hostName;
-    this.port = port;
+    if(userName == null){
+        throw new IllegalArgumentException();
+    } else {
+        this.userName = userName;
+    }
+        if(hostName == null){
+            throw new IllegalArgumentException();
+        } else {
+            this.hostName = hostName;
+        }
+        if(port < 1024){
+            throw new IllegalArgumentException();
+        } else {
+            this.port = port;
+        }
     this.closeConnection = false;
     }
     /**
